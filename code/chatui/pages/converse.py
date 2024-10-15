998b005dd22ede6f7bb97bb3f5770391fedb72eb
charur-nv <charu@nvidia.com>
2024-10-15 19:52:11 +0000

    Project Spec Modified & more...
    - Updated timeout_seconds in app 'jupyterlab'
    - Updated Requested GPUs to '2'
    - Modified files: code/chatui/pages/converse.py

--- code/chatui/pages/converse.py
+++ code/chatui/pages/converse.py
@@ -36,7 +36,7 @@ from chatui.pages import utils
 
 _LOGGER = logging.getLogger(__name__)
 PATH = "/"
-TITLE = "Hybrid RAG: Chat UI"
+TITLE = "Enterprise Hybrid RAG: Chat UI"
 OUTPUT_TOKENS = 250
 MAX_DOCS = 5
 
